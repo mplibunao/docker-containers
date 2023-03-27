04e1d5af9ca4c671f75eadab75ef47973861913d
MP <markpaololibunao@gmail.com>
2023-03-27 13:27:19 +0800

    Updates postgres initialization script to GRANT CREATE permissions to the public schema for the created user to make it work for postgres 15 and above

--- postgres/create_db.sh
+++ postgres/create_db.sh
@@ -9,9 +9,10 @@ function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database WITH PASSWORD '$POSTGRES_PASSWORD';
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+		CREATE USER $database WITH PASSWORD '$POSTGRES_PASSWORD';
+		CREATE DATABASE $database;
+		GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+		GRANT CREATE ON SCHEMA public TO $database
 EOSQL
 }
 
